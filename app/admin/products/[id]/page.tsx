e8401daea36faf8f9038ecec9a87fa9da4b200c3
Perumal <laabamonebusiness@gmail.com>
2025-12-23 15:22:50 +0530

    build issue fixed in product/[id] page

--- app/admin/products/[id]/page.tsx
+++ app/admin/products/[id]/page.tsx
@@ -140,15 +140,6 @@ export default function AdminProductViewPage() {
           <p className="text-gray-600">Loading product...</p>
         </div>
       </div>
-                    <div className="h-4 bg-gray-200 rounded"></div>
-                    <div className="h-4 bg-gray-200 rounded w-3/4"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     );
   }
 
